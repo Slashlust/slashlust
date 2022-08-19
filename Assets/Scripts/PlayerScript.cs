55f0c00ddc99e6d043431b0df95590a3db7aaa73
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-08-18 21:30:55 -0300

    Basic setup

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#nullable enable
+
+public class PlayerScript : MonoBehaviour
+{
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+}
