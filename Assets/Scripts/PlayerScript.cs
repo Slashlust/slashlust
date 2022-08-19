7114d77e4a55a7063c02c4d693ff47f9426a9d34
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-08-18 22:40:08 -0300

    Simple movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,10 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 #nullable enable
 
 public class PlayerScript : MonoBehaviour
 {
   CharacterController? controller;
+
+  Vector2 currentMoveInput;
+  Vector2 processedMoveInput;
+
+  void HandleMovement(CharacterController controller)
+  {
+    var targetMoveInput = currentMoveInput;
+
+    processedMoveInput = Vector2.Lerp(
+      processedMoveInput,
+      targetMoveInput,
+      .05f
+    );
+
+    var moveInput = processedMoveInput;
+
+    var move = moveInput.y * transform.forward + moveInput.x *
+      transform.right;
+
+    controller.SimpleMove(Vector3.ClampMagnitude(move, 1f) * 6f);
+  }
+
+  public void Move(InputAction.CallbackContext context)
+  {
+    var value = context.ReadValue<Vector2>();
+
+    currentMoveInput = value;
+  }
 
   void Awake()
   {
@@ -18,6 +47,9 @@ public class PlayerScript : MonoBehaviour
 
   void Update()
   {
-
+    if (controller != null)
+    {
+      HandleMovement(controller: controller);
+    }
   }
 }
