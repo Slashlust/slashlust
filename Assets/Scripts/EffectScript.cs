536145a7535e7555a428b20144e61af7b1dc8420
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-09-23 22:15:00 -0300

    Ajuste no effect script

--- Assets/Scripts/EffectScript.cs
+++ Assets/Scripts/EffectScript.cs
@@ -1,15 +1,16 @@
-using System.Collections.Generic;
 using UnityEngine;
-using Unity.AI.Navigation;
 
 #nullable enable
+
 class EffectScript : MonoBehaviour
 {
   System.Collections.IEnumerator DestroyRoutine()
   {
     yield return new WaitForSeconds(2f);
+
     Destroy(gameObject);
   }
+
   void Start()
   {
     StartCoroutine(DestroyRoutine());
